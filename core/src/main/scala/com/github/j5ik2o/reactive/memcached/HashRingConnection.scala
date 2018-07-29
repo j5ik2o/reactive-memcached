dc2ec5b21b896075b440f51ccae729c04f115b40
Junichi Kato <j5ik2o@gmail.com>
2018-07-29 13:31:36 +0900

    add logging

--- core/src/main/scala/com/github/j5ik2o/reactive/memcached/HashRingConnection.scala
+++ core/src/main/scala/com/github/j5ik2o/reactive/memcached/HashRingConnection.scala
@@ -5,9 +5,13 @@ import java.util.UUID
 import com.github.j5ik2o.reactive.memcached.command.CommandRequestBase
 import com.github.j5ik2o.reactive.memcached.util.HashRing
 import monix.eval.Task
+import org.slf4j.LoggerFactory
 
 final case class HashRingConnection(connectionPools: Seq[MemcachedConnectionPool[Task]], replicas: Int = 2)
     extends MemcachedConnection {
+
+  private val logger = LoggerFactory.getLogger(getClass)
+
   override def id: UUID = UUID.randomUUID()
 
   private lazy val hashRingConnections = HashRing(connectionPools.toList, replicas)
@@ -15,12 +19,23 @@ final case class HashRingConnection(connectionPools: Seq[MemcachedConnectionPool
   override def peerConfig: Option[PeerConfig] = None
 
   override def shutdown(): Unit = {
+    logger.debug("HashRingConnection#shutdown: start")
     connectionPools.foreach(_.dispose())
+    logger.debug("HashRingConnection#shutdown: finished")
   }
 
   private def key[C <: CommandRequestBase](cmd: C): Array[Byte] = cmd.key.getBytes(StandardCharsets.UTF_8)
 
-  override def send[C <: CommandRequestBase](cmd: C): Task[cmd.Response] =
-    hashRingConnections.getNode(key(cmd)).withConnectionF(_.send(cmd))
+  override def send[C <: CommandRequestBase](cmd: C): Task[cmd.Response] = {
+    logger.debug("HashRingConnection#send: start")
+    val connectionPool = hashRingConnections.getNode(key(cmd))
+    logger.debug("HashRingConnection#send: connectionPool = {}", connectionPool)
+    val result = connectionPool.withConnectionF { con =>
+      logger.debug("HashRingConnection#connectionPool#withConnectionF: con = {}", con)
+      con.send(cmd)
+    }
+    logger.debug("HashRingConnection#send: finished")
+    result
+  }
 
 }
