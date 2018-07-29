3d19dd291a281838105487b51a5409626a0cef3c
Junichi Kato <j5ik2o@gmail.com>
2018-07-29 13:34:10 +0900

    Fix ValueExpr

--- core/src/main/scala/com/github/j5ik2o/reactive/memcached/parser/model/ValueExpr.scala
+++ core/src/main/scala/com/github/j5ik2o/reactive/memcached/parser/model/ValueExpr.scala
@@ -1,3 +1,3 @@
 package com.github.j5ik2o.reactive.memcached.parser.model
 
-final case class ValueExpr(key: String, flags: Int, bytes: Long, casUnique: Option[Long], value: String ) extends Expr
+final case class ValueExpr(key: String, flags: Int, bytes: Long, casUnique: Option[Long], value: String) extends Expr
