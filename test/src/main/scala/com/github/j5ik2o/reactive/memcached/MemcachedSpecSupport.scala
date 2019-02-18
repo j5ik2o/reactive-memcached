6321d28480a2d9f20f21861bd5bf7060b53e6261
Junichi Kato <j5ik2o@gmail.com>
2019-02-18 13:50:22 +0900

    revised thread pool for test

--- test/src/main/scala/com/github/j5ik2o/reactive/memcached/MemcachedSpecSupport.scala
+++ test/src/main/scala/com/github/j5ik2o/reactive/memcached/MemcachedSpecSupport.scala
@@ -1,8 +1,10 @@
 package com.github.j5ik2o.reactive.memcached
 
+import java.util.concurrent.Executors
+
 import org.scalatest.{ BeforeAndAfterAll, Suite }
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ ExecutionContext, ExecutionContextExecutorService }
 
 @SuppressWarnings(
   Array("org.wartremover.warts.Null", "org.wartremover.warts.Var", "org.wartremover.warts.MutableDataStructures")
@@ -13,24 +15,24 @@ trait MemcachedSpecSupport extends Suite with BeforeAndAfterAll {
 
   def memcachedTestServer: MemcachedTestServer = _memcachedTestServer
 
+  override protected def beforeAll(): Unit = {
+    super.beforeAll()
+    implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
+    startMemcached()
+  }
+
   def startMemcached()(implicit ec: ExecutionContext): Unit = {
     _memcachedTestServer = new MemcachedTestServer()
     _memcachedTestServer.start()
   }
 
-  def stopMemached(): Unit = {
-    _memcachedTestServer.stop()
-  }
-
-  override protected def beforeAll(): Unit = {
-    super.beforeAll()
-    import scala.concurrent.ExecutionContext.Implicits.global
-    startMemcached()
-  }
-
   override protected def afterAll(): Unit = {
     stopMemached()
     super.afterAll()
+  }
+
+  def stopMemached(): Unit = {
+    _memcachedTestServer.stop()
   }
 
 }
